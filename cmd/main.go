53c187a5a5050c85f4bcce67efbee3a52133eeaf
Vladislavs Perkanuks <Vladislavs.Perkanuks@swisscom.com>
2025-09-26 21:22:04 +0300

    Connect go with DB and migrations

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,61 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 
+	"github.com/VladislavsPerkanuks/Entain-test-task/internal/config"
 	httpServer "github.com/VladislavsPerkanuks/Entain-test-task/internal/http"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+func migrateDB(conf *config.Config) {
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		conf.DB_USER, conf.DB_PASSWORD, conf.DB_HOST, conf.DB_PORT, conf.DB_NAME))
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to the database: %v", err))
+	}
+	defer db.Close()
+
+	// Test the connection
+	if err := db.Ping(); err != nil {
+		panic(fmt.Sprintf("Failed to ping database: %v", err))
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create postgres driver: %v", err))
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres", driver)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create migrate instance: %v", err))
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		panic(fmt.Sprintf("Failed to run migrations: %v", err))
+	}
+
+	fmt.Println("Database migrations completed successfully")
+}
+
 func main() {
+	serverConfig := config.DefaultConfig()
+
+	migrateDB(serverConfig)
+
 	router := httpServer.NewRouter()
 
-	fmt.Println("Starting server on :3000...")
-	if err := http.ListenAndServe(":3000", router); err != nil {
+	fmt.Printf("Starting server on :%s...\n", serverConfig.SERVER_PORT)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", serverConfig.SERVER_PORT), router); err != nil {
 		fmt.Printf("Server failed to start: %v\n", err)
 	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+type Config struct {
+	// DB
+	DB_HOST     string
+	DB_PORT     string
+	DB_USER     string
+	DB_PASSWORD string
+	DB_NAME     string
+
+	// Server
+	SERVER_PORT string
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		DB_HOST:     "localhost",
+		DB_PORT:     "5432",
+		DB_USER:     "postgres",
+		DB_PASSWORD: "password",
+		DB_NAME:     "database",
+		SERVER_PORT: "3000",
+	}
+}
